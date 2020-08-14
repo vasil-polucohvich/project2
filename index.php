8ae4131eba30d7f489f40f3a6dacf64512759797
vasil-poluhovich <super_vasil71@ukr.net>
2020-08-14 10:28:38 +0300

    commit file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, alias quod soluta!</h1>
+	
+</body>
+</html>
