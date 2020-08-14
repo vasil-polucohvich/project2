5a403ef0519c4f449ff82ba90f8421a3a9168c7d
vasil-poluhovich <super_vasil71@ukr.net>
2020-08-14 10:35:51 +0300

    commit2

---
+++ homapage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis tempora, vero a voluptatibus mollitia voluptas modi eveniet impedit officiis!</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam aperiam minus, sapiente!</p>
+</body>
+</html>
